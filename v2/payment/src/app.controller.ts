0fbaab1dce8ce77b5c721d108f779da534646465
Artem Vlasenko <s18b3_vlasenko@179.ru>
2022-10-25 19:28:23 +0300

    add base payment service

---
+++ v2/payment/src/app.controller.ts
@@ -0,0 +1,32 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Payment } from './payment';
+import { AppService } from './app.service';
+
+@Controller('service')
+export class AppController {
+  constructor(
+    private readonly service: AppService,
+) {}
+
+@Post('/')
+async createPayment(
+    @Body() body: Payment,
+)  {
+    return await this.service.createPayment(body); 
+}
+
+@Patch('/:paymentId') 
+async updatePayment(
+    @Body() body: Payment, 
+    @Param('paymentId') uid: string,
+) {
+    return await this.service.updatePaymentStatus(uid, body.status);
+}
+
+@Get('/:paymentId')
+async getPayment(
+    @Param('paymentId') uid: string,
+) {
+    return await this.service.getPaymentByUid(uid);
+}
+}
